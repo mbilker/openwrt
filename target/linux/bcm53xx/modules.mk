054b6d6f4863b8370e02c81203f836dc462dabbc
Matthew Hagan <mnhagan88@gmail.com>
2021-09-05 22:57:44 +0000

    bcm53xx: hwmon: add BCM59111 PSE support
    Add support for the Broadcom BCM59111 PSE. The driver with embedded
    fw image has been extracted from the Meraki MX64/MX65 GPL archive[1].
    
    Currently the only user of this is the Meraki MX65 so this has been
    added to the bcm53xx platform only.
    
    [1] https://dl.meraki.net/wired-14-39-mx64-20190426.tar.bz2
    
    Signed-off-by: Matthew Hagan <mnhagan88@gmail.com>
    Signed-off-by: Matthew Bilker <me@mbilker.us>

--- target/linux/bcm53xx/modules.mk
+++ target/linux/bcm53xx/modules.mk
@@ -50,3 +50,18 @@ define KernelPackage/i2c-bcm-iproc/description
 endef
 
 $(eval $(call KernelPackage,i2c-bcm-iproc))
+
+define KernelPackage/hwmon-bcm59111
+  TITLE:=Broadcom BCM59111 PSE support
+  KCONFIG:=CONFIG_SENSORS_BCM59111
+  DEPENDS:=@TARGET_bcm53xx +kmod-hwmon-core +kmod-i2c-core
+  SUBMENU:=$(HWMON_MENU)
+  FILES:=$(LINUX_DIR)/drivers/hwmon/bcm_poe.ko
+  AUTOLOAD:=$(call AutoLoad,60,bcm_poe,1)
+endef
+
+define KernelPackage/hwmon-bcm59111/description
+  Kernel module for the Broadcom BCM59111 PSE controller.
+endef
+
+$(eval $(call KernelPackage,hwmon-bcm59111))
