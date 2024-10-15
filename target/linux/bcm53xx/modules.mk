f40ffc8dfc919523eed8ff5fb91ab2a34e92a357
Matthew Bilker <me@mbilker.us>
2024-10-15 19:40:21 -0400

    bcm53xx: update setup from Leo-PL for testing

--- target/linux/bcm53xx/modules.mk
+++ target/linux/bcm53xx/modules.mk
@@ -51,23 +51,6 @@ endef
 
 $(eval $(call KernelPackage,i2c-bcm-iproc))
 
-define KernelPackage/switch-qca8k
-  SUBMENU:=$(NETWORK_DEVICES_MENU)
-  TITLE:=Qualcomm QCA8337 DSA switch driver
-  FILES:=$(LINUX_DIR)/drivers/net/dsa/qca/qca8k.ko
-  KCONFIG:=CONFIG_NET_DSA_QCA8K \
-	CONFIG_NET_DSA_TAG_QCA=y \
-	CONFIG_REGMAP=y
-  DEPENDS:=@TARGET_bcm53xx
-  AUTOLOAD:=$(call AutoLoad,50,qca8k)
-endef
-
-define KernelPackage/qca8k/description
-  Kernel module for Qualcomm QCA8337 DSA driver
-endef
-
-$(eval $(call KernelPackage,switch-qca8k))
-
 define KernelPackage/hwmon-bcm59111
   TITLE:=Broadcom BCM59111 PSE support
   KCONFIG:=CONFIG_SENSORS_BCM59111
